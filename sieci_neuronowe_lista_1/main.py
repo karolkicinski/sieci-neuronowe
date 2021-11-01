96f1b8919b4b87f83263f4c891673dac34214345
karolkicinski <karol.jakub.kicinski@gmail.com>
2021-11-01 15:31:15 +0100

    Lab1 - completed.

--- sieci_neuronowe_lista_1/main.py
+++ sieci_neuronowe_lista_1/main.py
@@ -5,7 +5,7 @@ import warnings
 from zad2 import Adaline
 
 
-def start_zad_1():
+def lab1_test():
     print("Hello")
 
     network = SimpleNeuralNetwork()
@@ -16,14 +16,84 @@ def start_zad_1():
     print(network.weights)
 
     print("Testing the data")
-    test_data = np.array([[0.0001, 0.00201], [0.002, 0.999], [1.022, 0.002], [1, 1], ])
+    test_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1], ])
 
     for data in test_data:
         print(f"Result for {data} is:")
         print(network.propagation(data))
 
 
-def start_zad_2():
+def lab1_report():
+    print("==========> ZAD. 1")
+
+    theta = [0.1, 0.3, 0.5, 0.9, 1, 1.1, 2, 5, 10, 20, 50, 100]
+
+    for t in theta:
+        epochs = []
+        for i in range(10):
+            network = SimpleNeuralNetwork()
+            network.DEBUG = False
+            network.is_bias = False
+            network.threshold = t
+            network.train()
+            epochs.append(network.epochs)
+
+        print(f"Theta = {t}, AV EPOCHS: {sum(epochs) / len(epochs)}")
+
+    # ==================================================================
+
+    print("==========> ZAD. 2")
+
+    weight_ranges = [[-1.0, 1.0], [-0.8, 0.8], [-0.5, 0.5], [-0.4, 0.4], [-0.3, 0.3],
+                     [-0.2, 0.2], [-0.1, 0.1], [-0.01, 0.01], [-0.001, 0.001], [-0.0001, 0.0001]]
+
+    for w in weight_ranges:
+        epochs = []
+        for i in range(10):
+            network = SimpleNeuralNetwork()
+            network.DEBUG = False
+            network.is_bias = True
+            network.weight_range = w
+            network.generate_weights()
+            network.train()
+            epochs.append(network.epochs)
+
+        print(f"Weight range = {w}, AV EPOCHS: {sum(epochs) / len(epochs)}")
+
+    # ==================================================================
+
+    print("==========> ZAD. 3")
+
+    alpha = [10, 5, 1.0, 0.5, 0.4, 0.2, 0.1, 0.01, 0.001, 0.0001]
+
+    for a in alpha:
+        epochs = []
+        for i in range(10):
+            network = SimpleNeuralNetwork()
+            network.DEBUG = False
+            network.is_bias = False
+            network.learning_factor = a
+            network.train()
+            epochs.append(network.epochs)
+
+        print(f"Alpha = {a}, AV EPOCHS: {sum(epochs) / len(epochs)}")
+
+    # ==================================================================
+
+    print("==========> ZAD. 4")
+
+    epochs = []
+    for i in range(10):
+        network = SimpleNeuralNetwork()
+        network.DEBUG = False
+        network.is_bias = False
+        network.train()
+        epochs.append(network.epochs)
+
+    print(f"AV EPOCHS: {sum(epochs) / len(epochs)}")
+
+
+def lab2_test():
     print("Hello in Adaline!\n")
 
     network = Adaline()
@@ -50,4 +120,5 @@ def start_zad_2():
 
 if __name__ == '__main__':
     warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
-    start_zad_2()
+    lab2_test()
+    # lab1_report()
--- sieci_neuronowe_lista_1/zad1.py
+++ sieci_neuronowe_lista_1/zad1.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 import random
 
@@ -5,57 +7,116 @@ import random
 class SimpleNeuralNetwork:
 
     def __init__(self):
-        self.weights = np.array([[random.uniform(-1.0, 1.0)/10, random.uniform(-1.0, 1.0)/10]]).T
+        self.DEBUG = True
+        self.weight_range = [-0.1, 0.1]
+        self.weights = np.array(0)
+
+        self.bias_weight = random.uniform(self.weight_range[0], self.weight_range[1])
+        self.bias_value = 1.0
+        self.is_bias = True
+
         self.threshold_uni = random.random()
         self.threshold_bi = random.uniform(-1.0, 1.0)
 
-        self.learning_factor = 0.1
+        self.threshold = self.threshold_uni
+        self.activate_function = self.activate_function_bi
+
+        self.learning_factor = 0.01
+        self.is_error = 1
+
+        self.epochs = 0
+
+        # self.train_inputs = np.array(
+        #     [[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0],
+        #      [0.00002, 0.001], [0.003, 1.00101], [1.0112, 0.00201], [1.0006, 1.00006]]
+        # )
+        # self.train_outputs = np.array([0, 0, 0, 1, 0, 0, 0, 1])
         self.train_inputs = np.array(
-            [[0, 0], [0, 1], [1, 0], [1, 1],
-             [0.0002, 0.00001], [0.003, 1.00101], [1.0112, 0.00201], [1.0006, 1.00006]]
+            [[-1.0, -1.0], [-1.0, 1.0], [1.0, -1.0], [1.0, 1.0],
+             [-1.00002, -1.001], [-1.003, 1.00101], [1.0112, -1.00201], [1.0006, 1.00006]]
         )
-        self.train_outputs = np.array([0, 0, 0, 1, 0, 0, 0, 1])
-        self.test_values = np.array([[0, 0], [0, 1], [1, 0], [1, 1], ])
+        self.train_outputs = np.array([-1, -1, -1, 1, -1, -1, -1, 1])
+
+        self.generate_weights()
+
+    def generate_weights(self):
+        self.weights = np.array([random.uniform(self.weight_range[0], self.weight_range[1]),
+                                 random.uniform(self.weight_range[0], self.weight_range[1])])
 
     def activate_function_uni(self, x):
-        output = np.empty(len(x))
-        for i in range(len(x)):
-            if x[i] > self.threshold_uni:
-                output[i] = 1
-            else:
-                output[i] = 0
-        return output
+        if self.is_bias:
+            threshold = 0
+        else:
+            threshold = self.threshold
+
+        if x > threshold:
+            return 1
+        else:
+            return 0
 
     def activate_function_bi(self, x):
-        output = np.empty(len(x))
-        for i in range(len(x)):
-            if x[i] > self.threshold_bi:
-                output[i] = 1
-            else:
-                output[i] = -1
-        return output
+        if self.is_bias:
+            threshold = 0
+        else:
+            threshold = self.threshold
+
+        if x > threshold:
+            return 1
+        else:
+            return -1
 
     def update_weights(self, result, offset):
-        output = np.empty(len(result))
+        error = self.train_outputs[offset] - result
         for i in range(len(self.weights)):
-            self.weights[i] = self.weights[i] + (self.train_outputs[offset] - result[0]) * self.train_inputs[offset][i] * self.learning_factor
-        return output
+            self.weights[i] += error * self.train_inputs[offset][i] * self.learning_factor
+        self.bias_weight += error * self.bias_value * self.learning_factor
+        self.is_error += math.fabs(error)
+
+    def pick_index(self, used):
+        if len(used) == len(self.train_inputs):
+            return False, -1
+        index = random.randint(0, len(self.train_inputs) - 1)
+        while index in used:
+            return self.pick_index(used)
+        return True, index
 
     def train(self):
-        while not self.check_values():
-            for i in range(len(self.train_inputs)):
-                propagation_result = self.propagation(self.train_inputs[i])
-                self.update_weights(propagation_result, i)
+        while self.is_error != 0:
+            self.is_error = 0
+            self.epochs += 1
+            used_indexes = []
+            result, index = self.pick_index(used_indexes)
 
-        print("end of training")
+            if self.epochs >= 10000:
+                print("Epochs out of range 10000!")
+                break
 
-    def propagation(self, inputs):
-        return self.activate_function_uni(np.dot(inputs.astype(float), self.weights))
+            while result:
+                propagation_result = self.propagation(self.train_inputs[index], self.is_bias)
+                self.update_weights(propagation_result, index)
 
-    def check_values(self):
-        for i in range(len(self.train_inputs)):
-            propagation_result = self.propagation(self.train_inputs[i])
+                used_indexes.append(index)
+                result, index = self.pick_index(used_indexes)
 
-            if propagation_result[0] != self.train_outputs[i]:
-                return False
-        return True
+        if self.DEBUG:
+            print("end of training")
+            print(f"epochs: {self.epochs}")
+
+    def propagation(self, inputs, bias=False):
+        if bias:
+            w = np.insert(self.weights, 0, self.bias_weight)
+            x = np.insert(inputs, 0, self.bias_value)
+        else:
+            w = self.weights
+            x = inputs
+        return self.activate_function(np.dot(x, w))
+
+    def print_result(self):
+        print("---------------------------------------------------")
+        print(f"Weights: {self.weights}")
+        print(f"Bias weight: {self.bias_weight}")
+        print(f"Is bias mode: {self.is_bias}")
+        print(f"Epochs: {self.epochs}")
+        print(f"Activate function: {self.activate_function.__name__}")
+        print(f"Threshold: {self.threshold}")
+        print("---------------------------------------------------")
