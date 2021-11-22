17878cc98afbe33d093c7923ad44a1ba475ec0f7
karolkicinski <karol.jakub.kicinski@gmail.com>
2021-11-22 22:27:41 +0100

    Lista 3 - init, Lista 2 - refactor.

--- lista3/Layer.py
+++ lista3/Layer.py
@@ -31,11 +31,15 @@ class Layer:
         self.input_size = input_size
         self.layer_size = layer_size
         self.weights = []
+        self.weights_predicted = []
+        self.momentum_saved_weights = []
+        self.weights_increase = []
         self.weights_update = []
         self.activation_function = activation_function
         self.stimulation = np.zeros(layer_size)
         self.activation = np.zeros(layer_size)
         self.error = 0
+        self.gradients_sum = []
 
         self.bias = 1
         self.bias_weight = np.zeros(layer_size)
@@ -43,14 +47,23 @@ class Layer:
         self.generate_weights()
 
     def generate_weights(self):
-        scale = 0.001
+        scale = 0.01
         self.weights = np.random.normal(scale=scale, size=(self.layer_size, self.input_size))
         self.bias_weight = np.random.normal(scale=scale, size=(self.layer_size, 1))
+        self.weights_increase = np.zeros(shape=(self.layer_size, self.input_size))
+        self.weights_predicted = self.weights
+        self.gradients_sum = np.zeros(shape=(self.layer_size, self.input_size))
 
-    def calculate_stimulation(self, x):
+    def calculate_stimulation(self, x, predicted=False):
         a = np.insert(x, 0, self.bias)
-        w = np.hstack((self.bias_weight, self.weights))
+        w = np.hstack((self.bias_weight, self.get_weights(predicted)))
         self.stimulation = np.dot(w, a)
 
     def proceed_activation(self):
         self.activation = self.activation_function(self.stimulation)
+
+    def get_weights(self, predicted=False):
+        if predicted:
+            return self.weights_predicted
+        else:
+            return self.weights
--- lista3/etap1.py
+++ lista3/etap1.py
@@ -11,11 +11,14 @@ class NeuralNetwork:
         self.layers = []
         self.learning_factor = 2
         self.epochs_limit = 800
-        self.batch_size = 1
+        self.batch_size = 16
 
         self.min_error_layers_states = []
         self.min_error = 1
         self.max_error_difference = 0.01
+
+        self.momentum = 0.7
+        self.momentum_nestrov = False
 
     def generate_layers(self):
         layers_sizes = [784, 10, 10]
@@ -28,7 +31,7 @@ class NeuralNetwork:
         last_result = x
 
         for j in range(len(self.layers)):
-            self.layers[j].calculate_stimulation(last_result)
+            self.layers[j].calculate_stimulation(last_result, self.momentum_nestrov)
             self.layers[j].proceed_activation()
             last_result = self.layers[j].activation
 
@@ -46,11 +49,11 @@ class NeuralNetwork:
         l = len(self.layers) - 2
         for i in range(l):
             lay_id = -1 - i
-            last_error = np.dot(self.layers[lay_id].weights.T, last_error) * self.layers[lay_id - 1].activation_function(self.layers[lay_id - 1].stimulation, True)
+            last_error = np.dot(self.layers[lay_id].get_weights(self.momentum_nestrov).T, last_error) * self.layers[lay_id - 1].activation_function(self.layers[lay_id - 1].stimulation, True)
             return_update.insert(0, np.outer(last_error, self.layers[lay_id - 2].activation))
 
         # layer first
-        last_error = np.dot(self.layers[1].weights.T, last_error) * self.layers[0].activation_function(self.layers[0].stimulation, True)
+        last_error = np.dot(self.layers[1].get_weights(self.momentum_nestrov).T, last_error) * self.layers[0].activation_function(self.layers[0].stimulation, True)
         return_update.insert(0, np.outer(last_error, x))
 
         return return_update
@@ -73,7 +76,7 @@ class NeuralNetwork:
         epoch = 0
         start_time = time.time()
         time_threshold = 60
-        acc = -1
+
         while self.epochs_limit > epoch and (time.time() - start_time) < time_threshold:
             iterator = 0
             update_grads = []
@@ -107,12 +110,29 @@ class NeuralNetwork:
             suma = 0.0
             for j in range(len(update_grads)):
                 suma += update_grads[j][i]
-            self.layers[i].weights -= self.learning_factor * suma / len(update_grads)
+            # ---> normal:
+            # self.layers[i].weights -= self.learning_factor * suma / len(update_grads)
+            # ---> momentum:
+            # weights_increase = self.apply_momentum(suma, i, len(update_grads))
+            # self.layers[i].weights -= self.learning_factor * weights_increase
+            # ---> momentum Nestrov (CHANGE FLAG self.momentum_nestrov!!!!!!!!!):
+            # self.layers[i].weights_predicted = self.layers[i].weights - self.momentum * self.layers[i].weights_increase
+            # ---> adagrad:
+            self.apply_adagrad(layer_id=i, weights_update=suma, batch_size=len(update_grads))
+
 
     def check_fit(self, x_val, y_val):
         lst = []
         for x, y in zip(x_val.values, y_val):
             lst.append(np.argmax(self.proceed_forward(x)) == np.argmax(y))
         return sum(lst) / len(lst)
+
+    def apply_momentum(self, update_grad, layer_id, batch_size):
+        self.layers[layer_id].weights_increase = self.momentum * self.layers[layer_id].weights_increase + self.learning_factor * update_grad / batch_size
+        return self.layers[layer_id].weights_increase
+
+    def apply_adagrad(self, layer_id, weights_update, batch_size, epsilon=1e-8):
+        self.layers[layer_id].gradients_sum += weights_update ** 2
+        self.layers[layer_id].weights -= (self.learning_factor * weights_update) / (np.sqrt(self.layers[layer_id].gradients_sum + epsilon) * batch_size)
 
 
