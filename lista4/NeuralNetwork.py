9203432b149e5fba501510a11431fa2af47c5186
karolkicinski <karol.jakub.kicinski@gmail.com>
2021-12-11 19:23:25 +0100

    Lista 4 - final

--- lista4/NeuralNetwork.py
+++ lista4/NeuralNetwork.py
@@ -6,12 +6,15 @@ from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, AveragePooling2D
 import matplotlib.pyplot as plt
 
 
-def generate_fully_connected():
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Flatten(input_shape=(28, 28)),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dense(10, activation='softmax')
-    ])
+def generate_fully_connected(layers):
+
+    model = Sequential()
+    model.add(Flatten(input_shape=(28, 28)))
+
+    for l in range(layers):
+        model.add(Dense(32, activation='relu'))
+
+    model.add(Dense(10, activation='softmax'))
 
     model.compile(
         optimizer='adam',
@@ -21,16 +24,22 @@ def generate_fully_connected():
     return model
 
 
-def generate_cnn():
-    filter_size = 3
-    pool_size = 2
+def generate_cnn(layers, filter_size, poolings, pool_size):
+    filter_size = filter_size
+    pool_size = pool_size
 
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Conv2D(32, kernel_size=filter_size, activation='relu', input_shape=(28, 28, 1)),
-        MaxPooling2D(pool_size=pool_size),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(10, activation='softmax')
-    ])
+    model = Sequential()
+
+    model.add(Conv2D(32, kernel_size=filter_size, activation='relu', input_shape=(28, 28, 1)))
+
+    for l in range(layers - 1):
+        model.add(Conv2D(16, kernel_size=filter_size, activation='relu'))
+
+    for p in range(poolings):
+        model.add(MaxPooling2D(pool_size=pool_size))
+
+    model.add(Flatten())
+    model.add(Dense(10, activation='softmax'))
 
     model.compile(
         optimizer='adam',
--- lista4/main.py
+++ lista4/main.py
@@ -1,3 +1,5 @@
+import csv
+
 import numpy as np
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
@@ -11,6 +13,48 @@ from tensorflow import keras
 FILE = "mnist.txt"
 
 
+def badania():
+    print("Start!")
+
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+    epochs = 5
+
+    parameter = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    iters = 10
+    list_epochs = []
+    list_errors = []
+    final_list_acc = []
+    final_list_param = []
+
+    with open('7.csv', 'w', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerow(['pooling size', 'epochs', 'accuracy'])
+
+        for p in parameter:
+            print(f"====================> PARAM VALUE: {p}")
+            results_acc = []
+            results_ep = []
+
+            for i in range(iters):
+                model = NeuralNetwork.generate_cnn(layers=1, filter_size=3, poolings=1, pool_size=p)
+                # model = NeuralNetwork.generate_fully_connected(layers=p)
+                model.fit(x_train, y_train, epochs=epochs)
+                acc = model.evaluate(x_test, y_test, verbose=2)
+
+                results_acc.append(acc[1])
+                results_ep.append(epochs)
+
+            final_list_acc.append(sum(results_acc)/len(results_acc))
+            final_list_param.append(p)
+            writer.writerow([p, round(sum(results_ep)/len(results_ep)), round(sum(results_acc)/len(results_acc), 4)])
+
+        plt.plot(final_list_param, final_list_acc)
+        plt.ylabel('Accuracy')
+        plt.xlabel('Pooling size')
+        plt.title(f"Badanie wpływu wielkości filtra dla pooling na szybkość uczenia sieci.")
+        plt.show()
+
+
 def start():
     print("Start!")
 
@@ -22,21 +66,24 @@ def start():
     model.fit(x_train, y_train, epochs=epochs)
     acc = model.evaluate(x_test, y_test, verbose=2)
 
-    print(f"Epochs: {epochs}, acc: {acc}!")
+    print(f"Epochs: {epochs}, acc: {round(acc[1] * 100, 2)}%!")
 
     print("End!")
 
 
-def save_mnist(x, y):
-    with open(FILE, mode="wb") as file:
-        pickle.dump((x, y), file)
+def pl():
+    plt.plot(
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        [0.9637, 0.9754, 0.9807, 0.9843, 0.9844, 0.9856, 0.9855, 0.9867, 0.9869, 0.9871]
+    )
+    plt.ylabel('Accuracy')
+    plt.xlabel('Layers')
+    plt.title(f"Badanie wpływu ilości warstw na szybkość uczenia sieci.")
+    plt.show()
 
-
-def load_mnist():
-    with open(FILE, mode="rb") as file:
-        (x, y) = pickle.load(file)
-        return x, y
 
 
 if __name__ == '__main__':
-    start()
+    # start()
+    badania()
+    # pl()
